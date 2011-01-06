e815e7dc08ab9ccc3f363a06717ca80fd3dae551
lukaslew <lukaslew@lukaslew-laptop.(none)>
2011-01-06 19:18:17 +0100

    init

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'thin'
+gem 'unicorn'
+gem 'nifty-generators'
+gem 'responders'
+gem 'simple_form'
+
+group :development do
+  gem 'wirble'
+  gem 'hirb'
+  gem 'jquery-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsciiArchSerwInt::Application.config.secret_token = 'fb75565a062a17ea97ef435550e279cfc821bed80b9709ef177c87b163b392b61b8b8d1839ed3c50d1521b18038c9f3abdba2d6a86111295feace858cfba7e56'
