4297b0e5ac004ea588e2c67235c8aa3296bb7e65
Łukasz Lewczyński <lukaslew@o2.pl>
2011-01-11 21:24:20 +0100

    User order is empty when session is created and everything in order is removed if user don't execute order

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -5,14 +5,31 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user_session = UserSession.new(params[:user_session])
+    @user_session = UserSession.new(params[:user_session])    
     @user_session.save
+    
+    @user = current_user
+    
+    if @user.orders.last != nil && @user.orders.last.buy == false
+    	@user.orders.delete(@user.orders.last)
+    end
+    
+    @user.orders << Order.new
+    @user.save
+    
     redirect_to root_url
   end
 
   def destroy
     @user_session = UserSession.find(params[:id])
     @user = current_user
+    @order = @user.orders.last
+    
+    if @order != nil && @order.buy == false
+    	@order.destroy
+    	@user.save
+    end
+    	
     @user_session.destroy
     redirect_to root_url
   end
